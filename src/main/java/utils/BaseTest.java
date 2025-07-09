25e18b5dcf63d638a3d1aa1697176c2d44b8e40c
Sush2699 <sgsushmita@gmail.com>
2025-07-09 10:17:33 +0530

    Update BaseTest.java

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -1,1 +1,24 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver(); // Requires chromedriver in PATH
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.com/ap/signin");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
 
